832dabbdc45a078b5ffa2b0bd733a59c3446a79e
Jungbey <1952325619@qq.com>
2021-11-19 15:17:24 +0800

    :building_construction: feat: detach app container

--- src/App.tsx
+++ src/App.tsx
@@ -2,45 +2,29 @@ import React, { useState } from 'react';
 import { css, setup } from 'goober';
 import { MainBody } from './components/main-body';
 import { Console } from './components/console';
-
-interface AppProps { }
+import AppContainer from './components/app_container';
 
 setup(React.createElement);
 
-const App: React.FC<AppProps> = () => {
+const App: React.FC = () => {
   const [currentLevel, setCurrentLevel] = useState<number>(0);
 
   return (
-    <div className={css`
-      min-width: 100vw;
-      min-height: 100vh;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    `}>
-      <div className={css`
-        height: 462px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: space-between;
-        position: relative;
-      `}>
-        <MainBody level={currentLevel} />
-        <div
-          className={css`
-            width: 26px;
-            height: 252px;
-            border: 1px solid #979797;
-            background-color: #D8D8D8;
-            margin: 0 auto;
-            position: absolute;
-            bottom: 35px;
-            z-index: -2;
-        `} />
-        <Console level={currentLevel} onLevelchange={setCurrentLevel} />
-      </div>
-    </div>
+    <AppContainer>
+      <MainBody level={currentLevel} />
+      <div
+        className={css`
+          width: 26px;
+          height: 252px;
+          border: 1px solid #979797;
+          background-color: #D8D8D8;
+          margin: 0 auto;
+          position: absolute;
+          bottom: 35px;
+          z-index: -2;
+      `} />
+      <Console level={currentLevel} onLevelchange={setCurrentLevel} />
+    </AppContainer>
   );
 }
 
