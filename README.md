e4b798164452baf43fdb879e0b42d42707f86e9e
SnowWarri0r <gerrytranCHINA@gmail.com>
2021-11-19 14:32:24 +0800

    �增加readme，格式化代码

---
+++ README.md
@@ -0,0 +1,3 @@
+# FAN  
+
+a visual fan,but with physical attributes
--- src/components/console/index.tsx
+++ src/components/console/index.tsx
@@ -11,52 +11,59 @@ interface ButtonProps {
   active: boolean;
 }
 
-const Button = styled("div")<ButtonProps>`
+const Button = styled('div')<ButtonProps>`
   width: 20px;
   height: 20px;
   border: 1px solid #979797;
   border-radius: 8px 8px 0 0;
-  background-color: #D8D8D8;
-  transform: translateY(${props => props.active ? 10 : 0}px);
-  transition: all .1s ease-in;
-`
+  background-color: #d8d8d8;
+  transform: translateY(${(props) => (props.active ? 10 : 0)}px);
+  transition: all 0.1s ease-in;
+`;
 
-let initialStatus: number[] = []
-for(let i = 0; i<5; i++) initialStatus.push(i);
+let initialStatus: number[] = [];
+for (let i = 0; i < 5; i++) initialStatus.push(i);
 
-export const Console = ({ level=0, onLevelchange }: ConsoleProps) => {
+export const Console = ({ level = 0, onLevelchange }: ConsoleProps) => {
   const a = useRef<number[]>(initialStatus);
 
   const handleChange = (index: number) => {
     onLevelchange(index);
-  }
+  };
 
-  return(
-    <div className={css`
-      width: 280px;
-      height: 35px;
-      margin: 0 auto;
-      background: url(${background}) center / 280px 35px no-repeat;
-      display: flex;
-      justify-content: center;
-    `}>
-      <div className={css`
-        width: 132px;
-        height: 20px;
-        overflow: hidden;
-        transform: translateY(-18px);
-      `}>
-        <div className={css`
-          display: flex;
-          justify-content: space-between;
-        `}>
-          {
-            a.current.map((index) => (
-              <Button active={index === level && index !== 0} onClick={() => handleChange(index)} />
-            ))
-          }
+  return (
+    <div
+      className={css`
+        width: 280px;
+        height: 35px;
+        margin: 0 auto;
+        background: url(${background}) center / 280px 35px no-repeat;
+        display: flex;
+        justify-content: center;
+      `}
+    >
+      <div
+        className={css`
+          width: 132px;
+          height: 20px;
+          overflow: hidden;
+          transform: translateY(-18px);
+        `}
+      >
+        <div
+          className={css`
+            display: flex;
+            justify-content: space-between;
+          `}
+        >
+          {a.current.map((index) => (
+            <Button
+              active={index === level && index !== 0}
+              onClick={() => handleChange(index)}
+            />
+          ))}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
